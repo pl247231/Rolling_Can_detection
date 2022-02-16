85c0d2058cc224b6a596d990dd6c7017eeb449f8
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-02-16 11:11:08 -0500

    Update rolling.py

--- rolling.py
+++ rolling.py
@@ -7,6 +7,7 @@ total = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
+
     if not ret:
         break
 
@@ -15,7 +16,7 @@ while cap.isOpened():
     blurred = cv.medianBlur(frame, 5)
     gray = cv.cvtColor(blurred, cv.COLOR_BGR2GRAY)
 
-    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 1000, param1=60, param2=25, minRadius=14, maxRadius=20)
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 1000, param1=70, param2=27, minRadius=14, maxRadius=20)
     if circles is not None:
         circles = np.uint16(np.around(circles))
         param = circles[0][0]
@@ -23,12 +24,15 @@ while cap.isOpened():
         cv.circle(frame, (param[0], param[1]), 2, (0, 255, 0), 3)
         detected += 1
 
-    cv.imshow('detected_video', np.hstack([frame, copy]))
+    cv.putText(copy, "Percentage: " + str(int(round(detected/total, 2)*100)) + "%", (150, 70), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1, cv.LINE_AA)
+    cv.imshow('video', np.hstack([frame, copy]))
+
     if cv.waitKey(5) == ord(' '):
         break
+
 print("Frames: " + str(total))
 print("Frames with circles: " + str(detected))
-print("Percentage: " + str(round(detected/total, 2)*100))
+print("Percentage: " + str(int(round(detected/total, 2)*100)) + "%")
 
 cap.release()
 cv.destroyAllWindows()
