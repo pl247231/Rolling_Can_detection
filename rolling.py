9acbbc2eda18b74ee08128b0d0cd35610248a4c5
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-02-16 11:10:00 -0500

    Update rolling.py

--- rolling.py
+++ rolling.py
@@ -4,25 +4,18 @@ import numpy as np
 cap = cv.VideoCapture('IMG_4616.MOV')
 detected = 0
 total = 0
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-    cv.imshow('raw_video', frame)
-    if cv.waitKey(25) == ord('q'):
-        break
-
-cap = cv.VideoCapture('IMG_4616.MOV')
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
+    copy = frame.copy()
     total += 1
     blurred = cv.medianBlur(frame, 5)
     gray = cv.cvtColor(blurred, cv.COLOR_BGR2GRAY)
 
-    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 1000, param1=60, param2=30, minRadius=5, maxRadius=20)
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 1000, param1=60, param2=25, minRadius=14, maxRadius=20)
     if circles is not None:
         circles = np.uint16(np.around(circles))
         param = circles[0][0]
@@ -30,8 +23,8 @@ while cap.isOpened():
         cv.circle(frame, (param[0], param[1]), 2, (0, 255, 0), 3)
         detected += 1
 
-    cv.imshow('detected_video', frame)
-    if cv.waitKey(25) == ord('q'):
+    cv.imshow('detected_video', np.hstack([frame, copy]))
+    if cv.waitKey(5) == ord(' '):
         break
 print("Frames: " + str(total))
 print("Frames with circles: " + str(detected))
