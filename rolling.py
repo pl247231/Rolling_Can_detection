f8e2172d5d6cfe1095bcdcc596523388df00aee6
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-02-16 11:11:56 -0500

    Update rolling.py

--- rolling.py
+++ rolling.py
@@ -4,7 +4,8 @@ import numpy as np
 cap = cv.VideoCapture('IMG_4616.MOV')
 detected = 0
 total = 0
-
+frame1 = 1
+copy1 = 1
 while cap.isOpened():
     ret, frame = cap.read()
 
@@ -24,11 +25,15 @@ while cap.isOpened():
         cv.circle(frame, (param[0], param[1]), 2, (0, 255, 0), 3)
         detected += 1
 
-    cv.putText(copy, "Percentage: " + str(int(round(detected/total, 2)*100)) + "%", (150, 70), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1, cv.LINE_AA)
+    cv.putText(frame, "Percentage:" + str(int(round(detected/total, 2)*100)) + "%", (300, 70), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv.LINE_AA)
+    cv.putText(frame, "Frames With Circles:" + str(detected), (50, 70), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv.LINE_AA)
     cv.imshow('video', np.hstack([frame, copy]))
-
+    frame1 = frame
+    copy1 = copy
     if cv.waitKey(5) == ord(' '):
         break
+cv.imshow('video', np.hstack([frame1, copy1]))
+cv.waitKey(0)
 
 print("Frames: " + str(total))
 print("Frames with circles: " + str(detected))
